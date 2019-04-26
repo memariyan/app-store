64222ab0b3107c0e97d29623e629fe8e85aaf9de
Tiyana <memariyan74@gmail.com>
2019-04-26 22:13:13 +0430

    add login and register pages designs

---
+++ app/src/main/java/ir/ac/iust/appstore/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package ir.ac.iust.appstore.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import ir.ac.iust.appstore.R;
+
+public class LoginActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
